d70e12eb24a8446d8fd5e3ee4ff4d84fe595c65c
kbdonnally <kbdonnally@gmail.com>
2021-09-02 17:53:25 -0400

    Clean up JS a little

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -8,39 +8,39 @@ let width 		= window.innerWidth;
 if (width < 1000) {
 	burgerIcon.onclick = () => {
 		burgerIcon.style.display = "none";
-		xIcon.style.display = "flex";
-		menuList.style.display = "flex";
+		xIcon.style.display 	 = "flex";
+		menuList.style.display 	 = "flex";
 	}
 	xIcon.onclick = () => {
-		xIcon.style.display = "none";
-		menuList.style.display = "none";
+		xIcon.style.display 	 = "none";
+		menuList.style.display 	 = "none";
 		burgerIcon.style.display = "flex";
 	}
 } else {
-	burgerIcon.style.display = "none";
-	xIcon.style.display = "none";
+	burgerIcon.style.display 	 = "none";
+	xIcon.style.display 		 = "none";
 }
 
 window.onresize = () => {
 	let width = window.innerWidth;
-	menuList.style.display = "none";
-	burgerIcon.style.display = "flex";
-	xIcon.style.display = "none;"
+	menuList.style.display 		 = "none";
+	burgerIcon.style.display 	 = "flex";
+	xIcon.style.display 	 	 = "none;"
 
 	if (width < 1000) {
 		burgerIcon.onclick = () => {
-			menuList.style.display = "flex";
-			xIcon.style.display = "flex";
+			menuList.style.display   = "flex";
+			xIcon.style.display 	 = "flex";
 			burgerIcon.style.display = "none";
 		}
 		xIcon.onclick = () => {
 			burgerIcon.style.display = "flex";
-			xIcon.style.display = "none";
-			menuList.style.display = "none";
+			xIcon.style.display 	 = "none";
+			menuList.style.display 	 = "none";
 		}
 	} else if (width => 1000) {
-		menuList.style.display = "flex";
+		menuList.style.display 	 = "flex";
 		burgerIcon.style.display = "none";
-		xIcon.style.display = "none";
+		xIcon.style.display 	 = "none";
 	}
 }
